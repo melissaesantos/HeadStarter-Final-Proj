3c545239265221ef09da8e5c76a26746d32e1a5b
Melissa Santos <melissasantos2202@gmail.com>
2024-08-10 13:57:49 -0600

    default message

--- chatsupport/src/app/page.js
+++ chatsupport/src/app/page.js
@@ -1,7 +1,11 @@
 'use client'
 import Image from "next/image";
- 
+import { useState } from "react";
 
 export default function Home() {
-  
+  const [messages,setMessages] = useState({
+    role: 'assistant',
+    content: 'Hi I am King Triton, here to help you navigate the seas of UC San Diego. How can I assist you today? '
+  })
+  const[message,setMessage] = useState('')
 }
